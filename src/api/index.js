50cac87bd2b30f93ab0cc632daee0baa768cf348
김동욱 <igerapex@gmail.com>
2019-12-19 20:05:23 +0900

    Fix api key constant value to env

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import { API_KEY } from '../constants';
+import dotenv from 'dotenv';
+dotenv.config();
+const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
 export const getMovies = page => {
   return axios.get('/discover/movie', {
     params: {
-      api_key: API_KEY,
+      api_key: REACT_APP_API_KEY,
       page,
       sort_by: 'primary_release_date.desc',
       'primary_release_date.lte': new Date()
@@ -17,7 +20,7 @@ export const getMovies = page => {
 export const getMovieDetails = id => {
   return axios.get(`/movie/${id}`, {
     params: {
-      api_key: API_KEY
+      api_key: REACT_APP_API_KEY
     }
   });
 };
@@ -25,7 +28,7 @@ export const getMovieDetails = id => {
 export const getMovieActors = id => {
   return axios.get(`/movie/${id}/credits`, {
     params: {
-      api_key: API_KEY
+      api_key: REACT_APP_API_KEY
     }
   });
 };
